feb72115c579795f10de5d250ec19a7515612392
Piper Wolters <piperw@allenai.org>
2023-12-18 16:13:33 -0800

    Code for OSM Object discriminator experiment.

---
+++ ssr/archs/osm_obj_discriminator_arch.py
@@ -0,0 +1,108 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from torch.nn.utils import spectral_norm
+
+from basicsr.utils.registry import ARCH_REGISTRY
+
+class SelfAttentionBlock(nn.Module):
+    def __init__(self, in_channels):
+        super(SelfAttentionBlock, self).__init__()
+        self.query_conv = nn.Conv2d(in_channels, in_channels // 8, kernel_size=1)
+        self.key_conv = nn.Conv2d(in_channels, in_channels // 8, kernel_size=1)
+        self.value_conv = nn.Conv2d(in_channels, in_channels, kernel_size=1)
+        self.gamma = nn.Parameter(torch.zeros(1))
+
+    def forward(self, x):
+        batch_size, channels, height, width = x.size()
+
+        # Calculate query, key, and value
+        proj_query = self.query_conv(x).view(batch_size, -1, width * height).permute(0, 2, 1)
+        proj_key = self.key_conv(x).view(batch_size, -1, width * height)
+        energy = torch.bmm(proj_query, proj_key)
+
+        attention = torch.nn.functional.softmax(energy, dim=2)
+        proj_value = self.value_conv(x).view(batch_size, -1, width * height)
+
+        out = torch.bmm(proj_value, attention.permute(0, 2, 1))
+        out = out.view(batch_size, channels, height, width)
+
+        out = self.gamma * out + x
+        return out
+
+
+@ARCH_REGISTRY.register()
+class OSMObjDiscriminator(nn.Module):
+    """
+    Arg:
+        num_in_ch (int): Channel number of inputs. Default: 3.
+        num_feat (int): Channel number of base intermediate features. Default: 64.
+        skip_connection (bool): Whether to use skip connections between U-Net. Default: True.
+    """
+
+    def __init__(self, num_in_ch, num_feat=64, skip_connection=True):
+        super(OSMObjDiscriminator, self).__init__()
+        self.skip_connection = skip_connection
+        norm = spectral_norm
+
+        # osm layers
+        self.o_conv1 = nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1)
+        self.o_conv2 = nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1)
+        self.o_attention1 = SelfAttentionBlock(128)
+        self.o_conv3 = nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1)
+        self.o_attention2 = SelfAttentionBlock(256)
+        self.o_conv4 = nn.Conv2d(256, 1, kernel_size=4, stride=2, padding=1)
+
+        # the first convolution
+        self.conv0 = nn.Conv2d(num_in_ch, num_feat, kernel_size=3, stride=1, padding=1)
+        # downsample
+        self.conv1 = norm(nn.Conv2d(num_feat, num_feat * 2, 4, 2, 1, bias=False))
+        self.conv2 = norm(nn.Conv2d(num_feat * 2, num_feat * 4, 4, 2, 1, bias=False))
+        self.conv3 = norm(nn.Conv2d(num_feat * 4, num_feat * 8, 4, 2, 1, bias=False))
+        # upsample
+        self.conv4 = norm(nn.Conv2d(num_feat * 8, num_feat * 4, 3, 1, 1, bias=False))
+        self.conv5 = norm(nn.Conv2d(num_feat * 4, num_feat * 2, 3, 1, 1, bias=False))
+        self.conv6 = norm(nn.Conv2d(num_feat * 2, num_feat, 3, 1, 1, bias=False))
+        # extra convolutions
+        self.conv7 = norm(nn.Conv2d(num_feat, num_feat, 3, 1, 1, bias=False))
+        self.conv8 = norm(nn.Conv2d(num_feat, num_feat, 3, 1, 1, bias=False))
+        self.conv9 = nn.Conv2d(num_feat, 1, 3, 1, 1)
+
+    def forward(self, x, osm_objs):
+
+        # osm layers
+        o1 = torch.relu(self.o_conv1(osm_objs))
+        o2 = torch.relu(self.o_conv2(o1))
+        o2_attn = self.o_attention1(o2)
+        o3 = torch.relu(self.o_conv3(o2_attn))
+        o3_attn = self.o_attention2(o3)
+        o_out = torch.relu(self.o_conv4(o3_attn))
+
+        # downsample
+        x0 = F.leaky_relu(self.conv0(x), negative_slope=0.2, inplace=True)
+        x1 = F.leaky_relu(self.conv1(x0), negative_slope=0.2, inplace=True)
+        x2 = F.leaky_relu(self.conv2(x1), negative_slope=0.2, inplace=True)
+        x3 = F.leaky_relu(self.conv3(x2), negative_slope=0.2, inplace=True)
+
+        # upsample
+        x3 = F.interpolate(x3, scale_factor=2, mode='bilinear', align_corners=False)
+        x4 = F.leaky_relu(self.conv4(x3), negative_slope=0.2, inplace=True)
+
+        if self.skip_connection:
+            x4 = x4 + x2
+        x4 = F.interpolate(x4, scale_factor=2, mode='bilinear', align_corners=False)
+        x5 = F.leaky_relu(self.conv5(x4), negative_slope=0.2, inplace=True)
+
+        if self.skip_connection:
+            x5 = x5 + x1
+        x5 = F.interpolate(x5, scale_factor=2, mode='bilinear', align_corners=False)
+        x6 = F.leaky_relu(self.conv6(x5), negative_slope=0.2, inplace=True)
+
+        if self.skip_connection:
+            x6 = x6 + x0
+
+        # extra convolutions
+        out = F.leaky_relu(self.conv7(x6), negative_slope=0.2, inplace=True)
+        out = F.leaky_relu(self.conv8(out), negative_slope=0.2, inplace=True)
+        out = self.conv9(out)
+        return out, o_out
