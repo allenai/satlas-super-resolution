e9075912ad6485b1ef8c72f2a2542f4f0be68e00
Piper Wolters <piperw@allenai.org>
2023-12-22 09:36:22 -0800

    gdal

--- install.sh
+++ install.sh
@@ -2,8 +2,12 @@
 apt-get update && apt-get install -y libgl1-mesa-dev
 apt-get update && apt-get install -y libxxf86vm1
 apt-get update && apt-get install ffmpeg libsm6 libxext6  -y
-apt-get update && apt-get install gdal-bin
-apt-get update && apt-get install libgdal-dev
+
+sudo add-apt-repository ppa:ubuntugis/ppa
+sudo apt-get update
+sudo apt-get install libgdal-dev gdal-bin
+export CPLUS_INCLUDE_PATH=/usr/include/gdal
+export C_INCLUDE_PATH=/usr/include/gdal
 
 pip install opencv-python
 pip install -r requirements.txt
