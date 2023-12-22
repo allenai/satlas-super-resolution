e911cdd0b290c4a762143625d054d2bc196eeed8
Piper Wolters <piperw@allenai.org>
2023-12-22 09:44:46 -0800

    Temporarily ignore gdal

--- install.sh
+++ install.sh
@@ -3,11 +3,5 @@ apt-get update && apt-get install -y libgl1-mesa-dev
 apt-get update && apt-get install -y libxxf86vm1
 apt-get update && apt-get install ffmpeg libsm6 libxext6  -y
 
-sudo add-apt-repository ppa:ubuntugis/ppa
-sudo apt-get update
-sudo apt-get install libgdal-dev gdal-bin
-export CPLUS_INCLUDE_PATH=/usr/include/gdal
-export C_INCLUDE_PATH=/usr/include/gdal
-
 pip install opencv-python
 pip install -r requirements.txt
