2c366572558ee0b496e592216de8fffc53de99bc
Piper Wolters <piperw@allenai.org>
2023-12-22 09:29:47 -0800

    Update install script

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 apt-get update && apt-get install -y libgl1-mesa-dev
 apt-get update && apt-get install -y libxxf86vm1
 apt-get update && apt-get install ffmpeg libsm6 libxext6  -y
+apt-get update && apt-get install gdal-bin
+apt-get update && apt-get install libgdal-dev
 
 pip install opencv-python
 pip install -r requirements.txt
